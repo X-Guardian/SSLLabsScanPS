6d029eea45d7235981edbe455bd07ce72d0e06e8
Simon Heather <simonheather99@gmail.com>
2020-07-04 18:08:50 +0100

    Update Get-SSLLabsScanEndpointData

--- source/Public/Get-SSLLabsScanEndpointData.ps1
+++ source/Public/Get-SSLLabsScanEndpointData.ps1
@@ -1,5 +1,4 @@
-function Get-SSLLabsScanEndpointData
-{
+function Get-SSLLabsScanEndpointData {
     <#
     .SYNOPSIS
         Gets the endpoint data for a scan
@@ -13,6 +12,9 @@ function Get-SSLLabsScanEndpointData
     .PARAMETER IPAddress
         Specifies the ip address of the host in the scan.
 
+    .PARAMETER HostData
+        Specifies the HostData object containing details of the scan
+
     .PARAMETER FromCache
         Specifies whether to retrieve the scan from the cache.
 
@@ -25,18 +27,28 @@ function Get-SSLLabsScanEndpointData
     .EXAMPLE
         Get-SSLLabsScanEndpointData -HostName www.bbc.co.uk -IPAddress 1.1.1.1
 
-        Gets the endpoint data for a scan
+        Gets the endpoint data for a scan on the bbc website for the specified IP address.
 #>
     [CmdletBinding(DefaultParameterSetName = 'Default')]
-    [OutputType( { ($script:ApiPropertes | Where-Object -Property ApiName -eq 'info').TypeName })]
+    [OutputType( { ($script:ApiPropertes | Where-Object -Property ApiName -eq 'getEndpointData').TypeName })]
     param(
-        [Parameter(Mandatory)]
+        [Parameter(
+            Mandatory,
+            ParameterSetName = 'Default')]
         [System.String]
         $HostName,
 
-        [Parameter(Mandatory)]
+        [Parameter(
+            Mandatory,
+            ParameterSetName = 'Default')]
         [System.String[]]
         $IPAddress,
+
+        [Parameter(
+            Mandatory,
+            ParameterSetName = 'HostData')]
+        [PSCustomObject]
+        $HostData,
 
         [Parameter()]
         [System.Management.Automation.SwitchParameter]
@@ -45,23 +57,26 @@ function Get-SSLLabsScanEndpointData
 
     $apiName = 'getEndpointData'
 
+    if ($PSCmdlet.ParameterSetName -eq 'HostData') {
+        $IPAddress = $HostData.endpoints.ipaddress
+        $HostName = $HostData.host
+    }
+
     $baseQueryParams = @()
 
     $baseQueryParams += "host=$HostName"
 
-    if ($PSBoundParameters.ContainsKey('FromCache'))
-    {
+    if ($PSBoundParameters.ContainsKey('FromCache')) {
         $baseQueryParams += 'fromCache=on'
     }
 
-    foreach ($ip in $IPAddress)
-    {
+    foreach ($ip in $IPAddress) {
         $queryParams = $baseQueryParams + "s=$ip"
         Write-Verbose "Getting SSL Labs Scan endpoint data on host $HostName, IP address $ip"
 
         $result = Invoke-SSLLabsScanApi -ApiName $apiName -QueryParameters $queryParams -Verbose:$false
 
-        $result | Add-Member -Name 'hostName' -Value $HostName -MemberType NoteProperty
+        $result | Add-Member -Name 'host' -Value $HostName -MemberType NoteProperty
         $result.details.hostStartTime = ([System.DateTimeOffset]::FromUnixTimeMilliSeconds($result.details.hostStartTime)).UtcDateTime
 
         return $result
